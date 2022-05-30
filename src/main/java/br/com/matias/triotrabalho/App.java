d376f3ba6cbf5e8a082004c396898065ed375a7a
Matias.Barbosa <matias.barbosa@gmail.com>
2022-05-30 11:39:47 -0300

    Corregir erro

--- src/main/java/br/com/matias/triotrabalho/App.java
+++ src/main/java/br/com/matias/triotrabalho/App.java
@@ -1,6 +1,6 @@
 package br.com.matias.triotrabalho;
 
-import java.util.Arrays;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
@@ -8,10 +8,15 @@ import java.time.format.DateTimeFormatter;
 public class App {
 
     public static void main(String[] args) {
+        try {
     Scanner sc = new Scanner(System.in);
     System.out.println("Digite a sua data de nascimento");
     String data = sc.next();
-    //LocalDate localdate = LocalDate.parse(data, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-    //System.out.println(localdate);
+    LocalDate localdate = LocalDate.parse(data, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        } catch(DateTimeParseException e) {
+            System.out.println("Formato errado");
+
+        }
     }
 }
