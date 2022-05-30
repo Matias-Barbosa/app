2caf777fd9b69f71c95dfe01c424bef189975a54
Matias.Barbosa <matias.barbosa@gmail.com>
2022-05-30 11:30:30 -0300

    trioTrabalho

---
+++ src/main/java/br/com/matias/triotrabalho/App.java
@@ -0,0 +1,17 @@
+package br.com.matias.triotrabalho;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class App {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite a sua data de nascimento");
+    String data = sc.next();
+    //LocalDate localdate = LocalDate.parse(data, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    //System.out.println(localdate);
+    }
+}
